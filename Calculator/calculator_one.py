e47c78ab4244be80fa561a28367fbf53e2ca895d
Christopher Forte <80968869+cnf2500@users.noreply.github.com>
2022-10-21 18:06:14 -0500

    Add files via upload

---
+++ Calculator/calculator_one.py
@@ -0,0 +1,27 @@
+#Author: Christopher Forte
+#Date: October 21, 2022
+#Title: Calculator (Python)
+#Description: First attempt at a basic calculator in Python.
+
+
+
+num1 = int(input("Enter First Operand: "))
+ch = input("Enter Operator: ")
+num2 = int(input("Enter Second Operand: "))
+
+output = 0 
+
+if ch == '+':
+  output = num1 + num2
+elif ch == '-':
+  output = num1 - num2
+elif ch == '*':
+  output = num1 * num2
+elif ch == '/':
+  output = num1 / num2
+elif ch == '%':
+  output = num1 % num2
+else:
+  print("Invalid Input")
+
+print(output)
